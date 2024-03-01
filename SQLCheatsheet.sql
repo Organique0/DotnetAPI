75299c3abb9c5710b7d3410bfcaf80cd5baf0e00
Luka Grabnar <grabnar.luka@gmail.com>
2024-03-02 00:17:38 +0100

    move SetPassword

---
+++ SQLCheatsheet.sql
@@ -0,0 +1,536 @@
+select [ComputerId],
+    [Motherboard],
+    ISNULL([CPUCores],4) as CPUCores,
+    [HasWifi],
+    [HasLTE],
+    [ReleaseDate],
+    [Price],
+    [VideoCard]
+from TutorialAppSchema.Computer
+ORDER BY HasWifi DESC, ReleaseDate DESC
+;
+
+SELECT [Users].[UserId],
+    [Users].[FirstName] + ' ' + [Users].[LastName] as FullName,
+    [Users].[Email],
+    [Users].[Gender],
+    [Users].[Active],
+    [UserJobInfo].[JobTitle],
+    [UserJobInfo].[Department]
+FROM TutorialAppSchema.Users as Users
+    JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+    ON UserJobInfo.UserId = Users.UserId
+WHERE Users.Active = 1
+ORDER BY Users.UserId DESC
+
+SELECT [Users].[UserId],
+    [Users].[FirstName] + ' ' + [Users].[LastName] as FullName,
+    [Users].[Email],
+    [Users].[Gender],
+    [Users].[Active],
+    [UserJobInfo].[JobTitle],
+    [UserJobInfo].[Department]
+FROM TutorialAppSchema.Users as Users
+    JOIN TutorialAppSchema.UserSalary as UserSalary
+    ON UserSalary.UserId = Users.UserId
+    LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+    ON UserJobInfo.UserId = Users.UserId
+WHERE Users.Active = 1
+ORDER BY Users.UserId DESC
+
+SELECT ISNULL([UserJobInfo].[Department], 'No department listed') as Department,
+    SUM([UserSalary].[Salary]) as Salary,
+    MIN([UserSalary].[Salary]) as minSalary,
+    MAX([UserSalary].[Salary]) as maxSalary,
+    AVG([UserSalary].[Salary]) as AvgSalary,
+    COUNT(*) as PeopleFromDepartment,
+    --list of all users ids in a department
+    STRING_AGG(Users.UserId, ', ') AS UserIds
+FROM TutorialAppSchema.Users as Users
+    JOIN TutorialAppSchema.UserSalary as UserSalary
+    ON UserSalary.UserId = Users.UserId
+    LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+    ON UserJobInfo.UserId = Users.UserId
+WHERE Users.Active = 1
+GROUP BY [UserJobInfo].[Department]
+ORDER BY UserJobInfo.Department DESC;
+
+SELECT [Users].[FirstName] + ' ' + [Users].[LastName] as FullName,
+    [Users].[Email],
+    [Users].[Gender],
+    [Users].[Active],
+    [UserJobInfo].[JobTitle],
+    [UserJobInfo].[Department],
+    DepartmentAverage.AvgSalary
+FROM TutorialAppSchema.Users as Users
+    JOIN TutorialAppSchema.UserSalary as UserSalary
+    ON UserSalary.UserId = Users.UserId
+    LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+    ON UserJobInfo.UserId = Users.UserId
+    --OUTER APPLY( --Similar to left join
+        CROSS APPLY
+( --Similar to join
+        SELECT ISNULL([UserJobInfo2].[Department], 'No department listed') as Department,
+        AVG([UserSalary2].[Salary]) as AvgSalary
+    FROM TutorialAppSchema.UserSalary as UserSalary2
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo2
+        ON UserJobInfo2.UserId = UserSalary2.UserId
+    WHERE ISNULL([UserJobInfo2].[Department], 'No department listed') = ISNULL([UserJobInfo].[Department], 'No department listed')
+    GROUP BY [UserJobInfo2].[Department]
+    )
+AS DepartmentAverage
+WHERE Users.Active = 1
+ORDER BY Users.UserId DESC
+
+--physically store table in the order of this id
+--makes the query run faster
+CREATE CLUSTERED INDEX cix_UserSalary_UserId ON TutorialAppSchema.UserSalary(UserId)
+
+--a new listing of just the salary in order and includes our clustered index
+CREATE NONCLUSTERED INDEX cix_UserSalary_UserId ON TutorialAppSchema.UserSalary(UserId)
+
+--also faster find department
+CREATE NONCLUSTERED INDEX ix_UserJobInfo_JobTitle ON TutorialAppSchema.UserJobInfo(JobTitle) INCLUDE (Department)
+
+--index with a where clause. When we search on the <active> field it includes <Email>, <FirstName> and <LastName>
+--it always includes the clustered index
+--Primary key is always a clustered index
+CREATE NONCLUSTERED INDEX ix_Users_JobTitle ON TutorialAppSchema.Users(Active) INCLUDE ([Email],[FirstName], [LastName])
+
+--cutout everything where active is 0 from the index
+CREATE NONCLUSTERED INDEX ix_Users_JobTitle ON TutorialAppSchema.Users(Active) 
+INCLUDE ([Email],[FirstName], [LastName])
+WHERE Active = 1;
+
+DELETE FROM TutorialAppSchema.UserSalary
+WHERE UserId BETWEEN 250 AND 750;
+
+--not equal: UserId <> 7
+
+--same as join
+SELECT *
+FROM TutorialAppSchema.UserSalary AS UserSalary
+WHERE EXISTS (SELECT *
+FROM TutorialAppSchema.UserJobInfo as UserJobInfo
+WHERE UserJobInfo.UserId = UserSalary.UserId AND UserId <> 7)
+
+--select only unique values from both datasets
+    SELECT [UserId],
+        [Salary]
+    from TutorialAppSchema.UserSalary
+UNION
+    SELECT [UserId],
+        [Salary]
+    from TutorialAppSchema.UserSalary
+
+--select all records from both tables
+    SELECT [UserId],
+        [Salary]
+    from TutorialAppSchema.UserSalary
+UNION ALL
+    SELECT [UserId],
+        [Salary]
+    from TutorialAppSchema.UserSalary
+
+
+SELECT GETDATE()
+SELECT DATEADD(YEAR, -5, GETDATE())
+
+SELECT DATEDIFF(MINUTE, DATEADD(YEAR, -5, GETDATE()), GETDATE());
+--Returns positive
+SELECT DATEDIFF(MINUTE, GETDATE(),DATEADD(YEAR, -5, GETDATE()));
+--Returns negative
+
+ALTER TABLE TutorialAppSchema.UserSalary ADD AvgSalary DECIMAL(18,4);
+-- add a blank field in a table
+
+/*
+basic stored procedure to select a user
+exec is to execute it
+*/
+CREATE PROCEDURE TutorialAppSchema.spUsers_Get
+    /*EXEC TutorialAppSchema.spUsers_Get @UserId=3*/
+    @UserId INT = NULL
+/*can be defaulted*/
+AS
+BEGIN
+    SELECT [UserId],
+        [FirstName],
+        [LastName],
+        [Email],
+        [Gender],
+        [Active]
+    FROM TutorialAppSchema.Users AS Users
+    /*if @UserId is null, then return all users*/
+    WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
+END
+
+
+--Using outer apply
+--Use alter in case it already exists
+ALTER PROCEDURE TutorialAppSchema.spUsers_Get
+    /*EXEC TutorialAppSchema.spUsers_Get @UserId=3*/
+    @UserId INT = NULL
+AS
+BEGIN
+    SELECT [Users].[UserId],
+        [FirstName],
+        [LastName],
+        [Email],
+        [Gender],
+        [Active],
+        UserSalary.Salary,
+        UserJobInfo.Department,
+        UserJobInfo.JobTitle,
+        AvgSalary.AvgSalary
+    FROM TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+        ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+        ON UserJobInfo.UserId = Users.UserId
+        OUTER APPLY (
+            SELECT AVG(UserSalary2.Salary) AvgSalary
+        FROM TutorialAppSchema.Users AS Users
+            LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary2
+            ON UserSalary2.UserId = Users.UserId
+            LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo2
+            ON UserJobInfo2.UserId = Users.UserId
+        WHERE UserJobInfo2.Department = UserJobInfo.Department
+        GROUP BY UserJobInfo2.Department
+        ) AS AvgSalary
+    WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
+END
+
+
+--store data in a temp table and join it to the query
+ALTER PROCEDURE TutorialAppSchema.spUsers_Get
+    /*EXEC TutorialAppSchema.spUsers_Get @UserId=3*/
+    @UserId INT = NULL
+AS
+BEGIN
+
+    SELECT Department,
+        AVG(UserSalary.Salary) AvgSalary
+    INTO #AverageDeptSalary
+    FROM TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+        ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+        ON UserJobInfo.UserId = Users.UserId
+    GROUP BY UserJobInfo.Department
+
+    CREATE CLUSTERED INDEX cix_AverageDeptSalary ON #AverageDeptSalary(Department);
+
+    SELECT [Users].[UserId],
+        [FirstName],
+        [LastName],
+        [Email],
+        [Gender],
+        [Active],
+        UserSalary.Salary,
+        UserJobInfo.Department,
+        UserJobInfo.JobTitle,
+        AvgSalary.AvgSalary
+    FROM TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+        ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+        ON UserJobInfo.UserId = Users.UserId
+        LEFT JOIN #AverageDeptSalary as AvgSalary
+        ON AvgSalary.Department = UserJobInfo.Department
+    WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
+END
+
+
+--drop the temp table if it already exists
+    IF OBJECT_ID('tempdb..#AverageSalary', 'U') IS NOT NULL
+    BEGIN
+    DROP TABLE #AverageDeptSalary
+END
+--works in newer mssql
+    DROP TABLE IF EXISTS #AverageDeptSalary
+
+
+--whole users get procedure: 
+ALTER PROCEDURE TutorialAppSchema.spUsers_Get
+    /*EXEC TutorialAppSchema.spUsers_Get @UserId=3*/
+    @UserId INT = NULL,
+    @Active BIT = NULL
+AS
+BEGIN
+    DROP TABLE IF EXISTS #AverageDeptSalary
+    SELECT Department,
+        AVG(UserSalary.Salary) AvgSalary
+    INTO #AverageDeptSalary
+    FROM TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+        ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+        ON UserJobInfo.UserId = Users.UserId
+    GROUP BY UserJobInfo.Department
+
+    CREATE CLUSTERED INDEX cix_AverageDeptSalary ON #AverageDeptSalary(Department);
+
+    SELECT [Users].[UserId],
+        [FirstName],
+        [LastName],
+        [Email],
+        [Gender],
+        [Active],
+        UserSalary.Salary,
+        UserJobInfo.Department,
+        UserJobInfo.JobTitle,
+        AvgSalary.AvgSalary
+    FROM TutorialAppSchema.Users AS Users
+        LEFT JOIN TutorialAppSchema.UserSalary AS UserSalary
+        ON UserSalary.UserId = Users.UserId
+        LEFT JOIN TutorialAppSchema.UserJobInfo AS UserJobInfo
+        ON UserJobInfo.UserId = Users.UserId
+        LEFT JOIN #AverageDeptSalary as AvgSalary
+        ON AvgSalary.Department = UserJobInfo.Department
+    WHERE Users.UserId = ISNULL(@UserId, Users.UserId)
+        AND ISNULL(Users.Active, 0) = COALESCE(@Active, Users.Active, 0)
+END
+
+
+--we insert a new user or update an existing one
+--create or alter works on newer versions
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spUser_Upsert
+    @FirstName NVARCHAR(50),
+    @LastName NVARCHAR(50),
+    @Email NVARCHAR(50),
+    @Gender NVARCHAR(50),
+    @JobTitle NVARCHAR(50),
+    @Department NVARCHAR(50),
+    @Salary DECIMAL(18,4),
+    @Active BIT = 1,
+    @UserId INT = NULL
+AS
+BEGIN
+    SELECT *
+    FROM TutorialAppSchema.UserSalary
+    SELECT *
+    FROM TutorialAppSchema.UserJobInfo
+
+    IF NOT EXISTS (SELECT *
+    FROM TutorialAppSchema.Users
+    WHERE UserId = @UserId)
+        BEGIN
+        DECLARE @OutputUserId INT
+        IF NOT EXISTS (SELECT *
+        FROM TutorialAppSchema.Users
+        WHERE Email = @Email)
+                BEGIN
+            INSERT INTO TutorialAppSchema.Users
+                (
+                [FirstName],
+                [LastName],
+                [Email],
+                [Gender],
+                [Active]
+                )
+            VALUES
+                (
+                    @FirstName,
+                    @LastName,
+                    @Email,
+                    @Gender,
+                    @Active
+                    )
+            --identity of the row inserted in the user table
+            SET @OutputUserId = @@IDENTITY
+
+            INSERT INTO TutorialAppSchema.UserSalary
+                (
+                UserId,
+                Salary
+                )
+            VALUES
+                (
+                    @OutputUserId,
+                    @Salary
+                    )
+
+            INSERT INTO TutorialAppSchema.UserJobInfo
+                (
+                UserId,
+                Department,
+                JobTitle
+                )
+            VALUES
+                (
+                    @OutputUserId,
+                    @Department,
+                    @JobTitle
+                    )
+        END
+    END
+    ELSE 
+        BEGIN
+        UPDATE TutorialAppSchema.Users 
+                SET FirstName = @FirstName,
+                    LastName = @LastName,
+                    Email = @Email,
+                    Gender = @Gender,
+                    Active = @Active
+                WHERE UserId = @UserId
+
+        UPDATE TutorialAppSchema.UserSalary 
+                SET Salary = @Salary
+                WHERE UserId = @UserId
+
+        UPDATE TutorialAppSchema.UserJobInfo 
+                SET Department = @Department,
+                    JobTitle = @JobTitle
+                WHERE UserId = @UserId
+    END
+END
+
+
+--delete a user procedure
+CREATE PROCEDURE TutorialAppSchema.spUser_Delete
+    @userId INT
+AS
+BEGIN
+    DELETE FROM TutorialAppSchema.Users
+        WHERE UserId = @userId
+    DELETE FROM TutorialAppSchema.UserSalary
+        WHERE UserId = @userId
+    DELETE FROM TutorialAppSchema.UserJobInfo
+        WHERE UserId = @userId
+END
+
+--1. get all posts of user and matching by SearchValue
+--2. get post by post id
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spPosts_Get
+    -- EXEC TutorialAppSchema.spPosts_Get @UserId = 1006, @SearchValue = 'hello'
+    -- EXEC TutorialAppSchema.spPosts_Get @PostId = 2
+
+    @UserId INT = NULL,-- =NULL means that it is nullable
+    @SearchValue NVARCHAR(MAX) = NULL,
+    @PostId INT = NULL
+AS
+BEGIN
+    SELECT [Posts].[PostId],
+        [Posts].[UserId],
+        [Posts].[PostTitle],
+        [Posts].[PostContent],
+        [Posts].[PostCreated],
+        [Posts].[PostUpdated]
+    FROM TutorialAppSchema.Posts AS Posts
+    WHERE Posts.UserId = ISNULL(@UserId, Posts.UserId)
+        AND Posts.PostId = ISNULL(@PostId, Posts.PostId)
+        AND (@SearchValue IS NULL
+        OR Posts.PostContent LIKE '%'+ @SearchValue +'%'
+        OR Posts.PostTitle LIKE '%'+ @SearchValue +'%'
+        )
+END
+
+
+
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spPosts_Upsert
+    @UserId INT,
+    @PostTitle NVARCHAR(255),
+    @PostContent NVARCHAR(MAX),
+    @PostId INT = NULL
+AS
+BEGIN
+    IF NOT EXISTS (SELECT *
+    FROM TutorialAppSchema.Posts
+    WHERE PostId = @PostId)
+        BEGIN
+        INSERT INTO TutorialAppSchema.Posts
+            (
+            [UserId],
+            [PostTitle],
+            [PostContent],
+            [PostCreated],
+            [PostUpdated]
+            )
+        VALUES
+            (
+                @UserId,
+                @PostTitle,
+                @PostContent,
+                GETDATE(),
+                GETDATE()
+            )
+    END
+    ELSE 
+        BEGIN
+        UPDATE TutorialAppSchema.Posts
+                SET PostTitle = @PostTitle,
+                    PostContent = @PostContent,
+                    PostUpdated = GETDATE()
+                WHERE PostId = @PostId
+    END
+END
+
+
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spPost_Delete
+    @PostId INT,
+    @UserId INT
+AS
+BEGIN
+    DELETE FROM TutorialAppSchema.Posts 
+    WHERE PostId = @PostId
+        AND UserId = @UserId
+END
+
+
+CREATE OR ALTER PROCEDURE TutorialAppSchema.spRegistration_Upsert
+    @Email NVARCHAR(50),
+    @PasswordHash VARBINARY(MAX),
+    @PasswordSalt VARBINARY(MAX)
+AS
+BEGIN
+    IF NOT EXISTS (SELECT *
+    FROM TutorialAppSchema.Auth
+    WHERE Email = @Email)
+        BEGIN
+        INSERT INTO TutorialAppSchema.Auth
+            (
+            [Email],
+            [PasswordHash],
+            [PasswordSalt]
+            )
+        VALUES
+            (
+                @Email,
+                @PasswordHash,
+                @PasswordSalt
+            )
+    END
+    ELSE
+        BEGIN
+        UPDATE TutorialAppSchema.Auth
+            SET PasswordHash = @PasswordHash,
+                PasswordSalt = @PasswordSalt
+            WHERE Email = @Email
+    END
+END
+GO
+
+CREATE OR ALTER PROCEDURE TutorialAppSchema.sp_LoginConfirmation_Get
+    @Email NVARCHAR(50)
+AS
+BEGIN
+    SELECT [Auth].[PasswordHash],
+        [Auth].[PasswordSalt]
+    FROM TutorialAppSchema.Auth AS Auth
+    WHERE Email = @Email
+END
+
+
+CREATE OR ALTER PROCEDURE TutorialAppSchema.sp_UserId_Get
+    @UserId INT = NULL,
+    @Email NVARCHAR(50) = NULL
+AS
+BEGIN
+    SELECT UserId
+    FROM TutorialAppSchema.Users
+    WHERE
+        (@UserId IS NULL OR UserId = @UserId)
+        AND
+        Email = ISNULL(@Email, Email)
+END
--- helpers/AuthHelper.cs
+++ helpers/AuthHelper.cs
@@ -1,7 +1,13 @@
+using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
+using DotnetAPI.Controllers;
+using DotnetAPI.Data;
+using DotnetAPI.Dtos;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Tokens;
 
 namespace DotnetAPI.Helpers
@@ -9,9 +15,12 @@ namespace DotnetAPI.Helpers
     public class AuthHelper
     {
         private readonly IConfiguration _config;
+        private readonly DataContextDapper _dapper;
+
         public AuthHelper(IConfiguration config)
         {
             _config = config;
+            _dapper = new DataContextDapper(config);
         }
 
         public byte[] GetPasswordHash(string pass, byte[] passSalt)
@@ -63,6 +72,48 @@ namespace DotnetAPI.Helpers
 
             return tokenHandler.WriteToken(token);
 
+        }
+
+        public bool SetPassword(UserForLoginDTO userForSetPass)
+        {
+            byte[] passwordSalt = new byte[128 / 8];
+            using (RandomNumberGenerator r = RandomNumberGenerator.Create())
+            {
+                r.GetNonZeroBytes(passwordSalt);
+            }
+
+            byte[] passwordHash = GetPasswordHash(userForSetPass.Password, passwordSalt);
+
+            //inserting new value in auth table
+            string sqlAddAuth = @"EXEC TutorialAppSchema.spRegistration_Upsert
+                        @Email = @EmailParam,
+                        @PasswordHash = @PasswordHashParam,
+                        @PasswordSalt = @PasswordSaltParam
+                    ";
+
+            //a diffent approach of passing params is needed here
+            //because of wierd types for pass hash and salt
+            //also, you can use DynamicParameters here instead with some modifications
+            List<SqlParameter> sqlParameters = [];
+
+            SqlParameter passwordHashParam = new("@PasswordHashParam", SqlDbType.VarBinary)
+            {
+                Value = passwordHash
+            };
+            SqlParameter passwordSaltParam = new("@PasswordSaltParam", SqlDbType.VarBinary)
+            {
+                Value = passwordSalt
+            };
+            SqlParameter email = new("@EmailParam", SqlDbType.NVarChar)
+            {
+                Value = userForSetPass.Email
+            };
+
+            sqlParameters.Add(passwordSaltParam);
+            sqlParameters.Add(passwordHashParam);
+            sqlParameters.Add(email);
+
+            return _dapper.ExecuteSqlWithParameters(sqlAddAuth, sqlParameters);
         }
     }
 }
